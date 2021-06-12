b5b871c65b02d2992daf1c5e70ab861e37c3fbb5
Ole Krüger <ole@vprsm.de>
2021-06-12 19:01:06 +0100

    Move api_resource module

--- src/k8s.rs
+++ src/k8s.rs
@@ -1,61 +1,19 @@
 pub mod lock;
 pub use lock::*;
 
+pub mod api_resource;
+pub use api_resource::*;
+
 use crate::meta::CRATE_VERSION;
 use futures::StreamExt;
 use futures::TryStreamExt;
 use kube::api::ListParams;
 use kube::api::WatchEvent;
-use kube::core::ApiResource;
-use kube::core::DynamicObject;
-use kube::core::GroupVersionKind;
-use kube::core::TypeMeta;
 use kube::Api;
 use kube::ResourceExt;
 use serde::de::DeserializeOwned;
 use serde::Serialize;
 use std::fmt::Debug;
-
-fn split_api_version(api_version: &str) -> (&str, &str) {
-    if let Some((group, version)) = api_version.split_once('/') {
-        (group, version)
-    } else {
-        ("", api_version)
-    }
-}
-
-pub trait ToApiResource {
-    fn to_resource(&self) -> ApiResource;
-}
-
-impl ToApiResource for ApiResource {
-    fn to_resource(&self) -> ApiResource {
-        self.clone()
-    }
-}
-
-impl ToApiResource for TypeMeta {
-    fn to_resource(&self) -> ApiResource {
-        let (group, version) = split_api_version(self.api_version.as_str());
-        ApiResource::from_gvk(&GroupVersionKind::gvk(group, version, self.kind.as_str()))
-    }
-}
-
-pub trait TryToApiResource {
-    fn try_to_api_resource(&self) -> Option<ApiResource>;
-}
-
-impl TryToApiResource for ApiResource {
-    fn try_to_api_resource(&self) -> Option<ApiResource> {
-        Some(self.clone())
-    }
-}
-
-impl TryToApiResource for DynamicObject {
-    fn try_to_api_resource(&self) -> Option<ApiResource> {
-        self.types.as_ref().map(|types| types.to_resource())
-    }
-}
 
 const TYPE_LABEL: &'static str = "able-seaman/type";
 const VERSION_LABEL: &'static str = "able-seaman/version";
